3435d041567a12de37e9211f95fb3df439e6364a
Caina Pimenta <08137abb-f9ec-419e-9d3b-21e517d31087>
2025-09-26 19:11:24 +0000

    Commit inicial

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/alfa/appmodeloui5/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/alfa/appmodeloui5/test/unit/controller/Products.controller"
+], function () {
+	"use strict";
+});
